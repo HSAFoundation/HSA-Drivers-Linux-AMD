7ca322ca5bf87a4982f25e94ff32e38037644c18
Oded Gabbay <oded.gabbay@amd.com>
2015-05-27 18:32:28 +0300

    Update kfd_check_installation.sh to detect CZ
    Signed-off-by: Oded Gabbay <oded.gabbay@amd.com>

--- kfd_check_installation.sh
+++ kfd_check_installation.sh
@@ -16,6 +16,7 @@ usage()
 __scriptname="kfd_check_installation.sh"
 __pass_flag="YES"
 kv_supported_types="1304 1305 1306 1307 1309 130a 130b 130c 130d 130e 130f 1310 1311 1312 1313 1315 1316 1317 1318 131b 131c 131d"
+cz_supported_types="9874"
 kfd_supported_perms="666 777"
 
 # parameter while-loop
@@ -52,20 +53,47 @@ if [[ $kv_exists != "0" ]]; then
 else
 	kv_exists_result="NO"
 	kv_type_result="N/A"
-	__pass_flag="NO"
+
+	cz_exists_result="NO"
+	for i in $cz_supported_types; do
+		cz_exists=$(lspci -nn | grep -c "1002:$i")
+		if [[ $cz_exists != "0" ]]; then
+			cz_exists_result="Yes"
+			cz_type_result="Yes"
+		fi
+	done
+
+	if [[ $cz_exists_result == "NO" ]]; then
+		cz_type_result="N/A"
+		__pass_flag="NO"
+	fi
 fi
 
 radeon_exists=$(grep -c -w radeon_pci_probe /proc/kallsyms)
+amdgpu_exists=$(grep -c -w amdgpu_pci_probe /proc/kallsyms)
 kfd_exists=$(grep -c -w kgd2kfd_init /proc/kallsyms)
 iommu_exists=$(grep -c -w amd_iommu_bind_pasid /proc/kallsyms)
 
-if [[ $radeon_exists == "1" ]]; then
-	radeon_exists_result="Yes"
-	radeon_blacklisted="0"
-else
-	radeon_exists_result="NO"
-	__pass_flag="NO"
-	radeon_blacklisted=$(grep blacklist /etc/modprobe.d/* | grep -c -w radeon)
+if [[ $kv_exists_result == "Yes" ]]; then
+	if [[ $radeon_exists == "1" ]]; then
+		radeon_exists_result="Yes"
+		radeon_blacklisted="0"
+	else
+		radeon_exists_result="NO"
+		radeon_blacklisted=$(grep blacklist /etc/modprobe.d/* | grep -c -w radeon)
+		__pass_flag="NO"
+	fi
+fi
+
+if [[ $cz_exists_result == "Yes" ]]; then
+	if [[ $amdgpu_exists == "1" ]]; then
+		amdgpu_exists_result="Yes"
+		amdgpu_blacklisted="0"
+	else
+		amdgpu_exists_result="NO"
+		amdgpu_blacklisted=$(grep blacklist /etc/modprobe.d/* | grep -c -w amdgpu)
+		__pass_flag="NO"
+	fi
 fi
 
 if [[ $kfd_exists == "1" ]]; then
@@ -112,11 +140,24 @@ fi
 # Print results
 
 echo -e ""
-echo -e "Kaveri detected:............................$kv_exists_result"
-echo -e "Kaveri type supported:......................$kv_type_result"
-echo -e "radeon module is loaded:....................$radeon_exists_result"
-if [[ ! $radeon_blacklisted == "0" ]]; then
-	echo -e "Radeon module is blacklisted!!!"
+if [[ $kv_exists_result == "Yes" ]]; then
+	echo -e "Kaveri detected:............................$kv_exists_result"
+	echo -e "Kaveri type supported:......................$kv_type_result"
+	echo -e "radeon module is loaded:....................$radeon_exists_result"
+	if [[ ! $radeon_blacklisted == "0" ]]; then
+		echo -e "Radeon module is blacklisted!!!"
+	fi
+else
+	echo -e "Carrizo detected:...........................$cz_exists_result"
+	if [[ $cz_exists_result == "Yes" ]]; then
+		echo -e "Carrizo type supported:.....................$cz_type_result"
+		echo -e "amdgpu module is loaded:....................$amdgpu_exists_result"
+		if [[ ! $amdgpu_blacklisted == "0" ]]; then
+			echo -e "amdgpu module is blacklisted!!!"
+		fi
+	else
+		echo -e "Kaveri detected:............................$kv_exists_result"
+	fi
 fi
 echo -e "amdkfd module is loaded:....................$kfd_exists_result"
 echo -e "AMD IOMMU V2 module is loaded:..............$iommu_exists_result"
